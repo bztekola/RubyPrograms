e99a641a9c65f8c6dfa891b4f1b2d6857a1523a2
bztekola <32548529+bztekola@users.noreply.github.com>
2017-10-10 11:03:15 -0400

    Create ppr_rk_szrs.rb

---
+++ ppr_rk_szrs.rb
@@ -0,0 +1,42 @@
+def calls
+	@rock = ""
+	@paper = ""
+	@scissors = ""
+end
+
+def win_combos
+	[@rock, @scissors],
+	[@scissors, @paper],
+	[@paper, @rock]
+end
+
+if win_combos == true
+
+
+
+
+
+
+class Players
+	initialize (aName, aDescription, afirst_move)
+		@aDescription = aDescription
+		@aName = aName
+		@afirst_move = afirst_move 
+
+
+
+
+
+def run_game
+	puts "Lurch  Millie  PTeet"
+	puts "Choose Arch Nemesis:"
+	opp = gets().chomp
+
+	while true
+	end
+end
+
+Lurch = Players.new (Lurch, A square-jawed barrel-chested Man, Rock)
+Millie = Players.new (Millie, An adequate mediocatress of the mean, Scissors)
+PTeet =Players.new (PTeet, A small man with little hands, Paper)
+
